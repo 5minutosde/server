8bddace9d0765e179ef79d122a8986dc3151ab28
Guilherme Guerra <guilherme.guerra@ilegra.com>
2019-04-05 16:08:32 -0300

    Adds function to handle messages

--- fivemin.py
+++ fivemin.py
@@ -70,6 +70,25 @@ def user_photo(photo_id, username, user_id):
     return media_url
 
 
+def handle_message(message, username, user_id, user_avatar):
+    if message['reply_to_message']:
+        return reply_message(message, username)
+    if message.media:
+        return handle_media(message, username, user_id)
+    slug = slugify(message["text"] + '-por-' + username)
+    data = {
+        "title": message["text"],
+        "slug": slug,
+        "created_at": str(datetime.now()),
+        "user": {
+            "id": user_id,
+            "username": username,
+            "avatar": user_avatar
+        }
+    }
+    db.child("audios/{}".format(message['message_id'])).set(data)
+
+
 @app.on_message()
 def my_handler(client, message):
 
@@ -77,36 +96,22 @@ def my_handler(client, message):
     username = message['from_user'].username
     user_id = message['from_user'].id
     photo_id = message['from_user'].photo.big_file_id
-
+    print(str(user_id) in allowed_users)
     if str(user_id) in allowed_users:
         user = db.child("users/{}".format(user_id)).get().val()
         if user:
-            if message['reply_to_message']:
-                return reply_message(message, username)
-            if message.media:
-                return handle_media(message, username, user_id)
-
-            slug = slugify(message["text"] + '-por-' + username)
-            data = {
-                "title": message["text"],
-                "slug": slug,
-                "created_at": str(datetime.now()),
-                "user": {
-                    "id": user_id,
-                    "username": username,
-                    "avatar": user['avatar']
-                }
-            }
-            db.child("audios/{}".format(message['message_id'])).set(data)
+            handle_message(message, username, user_id, user['avatar'])
         else:
             data = {
                 "username": username,
             }
             db.child("users/{}".format(user_id)).set(data)
-            user_photo(photo_id, username, user_id)
+            avatar_url = user_photo(photo_id, username, user_id)
+            handle_message(message, username, user_id, avatar_url)
     else:
         data = {
             "username": username,
+            "first_name": message['from_user'].first_name,
         }
         db.child("wait_list/{}".format(user_id)).set(data)
 
